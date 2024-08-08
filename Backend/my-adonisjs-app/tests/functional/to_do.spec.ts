8a426c0c1457a6ce2b6ed841b23a32880d04b39c
Pedro Vieira <pedrovieirafdev@gmail.com>
2024-08-08 18:36:37 -0300

    modificação no teste

--- Backend/my-adonisjs-app/tests/functional/to_do.spec.ts
+++ Backend/my-adonisjs-app/tests/functional/to_do.spec.ts
@@ -1,18 +1,37 @@
 import { test } from '@japa/runner'
+import { assert } from 'console'
 // import { ToDoFactory } from '#database/factories/to_do_factory'
 
-test.group('To Do', (group) => {
+test.group('To Do', (group: { tap: (arg0: (t: any) => void) => void }) => {
   group.tap((t) => {
     t.options.title = `it ${t.options.title}`
   })
 
-  test('tests the GET route', async ({ client }) => {
+  test('tests the GET route', async ({ client }: { client: any }) => {
     const response = await client.get('/toDos')
 
+    response.assertBodyContains([
+      {
+        "title": "Levar a vó no Jiu-Jitsu",
+        "description": "Levar a vó na quarta-feira às 19h",
+        "isCompleted": 0
+      },
+      {
+        "title": "Comprar pão",
+        "description": "Comprar pão na padaria do seu Zé",
+        "isCompleted": 1
+      },
+      {
+        "title": "Sem ideias",
+        "description": null,
+        "isCompleted": 0
+      }
+    ])
+    response.assert.exists(response.body)
     response.assertStatus(200)
   })
 
-  test('should test the show route', async ({ client }) => {
+  test('should test the show route', async ({ client }: { client: any }) => {
     const response = await client.get('/toDos/1')
 
     response.assertStatus(200)
@@ -23,7 +42,7 @@ test.group('To Do', (group) => {
     })
   })
 
-  test('should test the create route', async ({ client }) => {
+  test('should test the create route', async ({ client }: { client: any }) => {
     const response = await client.post('/toDos').form({
       title: 'tomara que funcione',
       description: 'frase do Ez',
@@ -36,23 +55,23 @@ test.group('To Do', (group) => {
     })
   })
 
-  test('should test the edit route', async ({ client }) => {
+  test('should test the edit route', async ({ client }: { client: any }) => {
     const response = await client.put('/toDos/3').form({
-      title: 'Com ideia!',
+      title: 'Com ideias!',
       description: 'genial demais',
       isCompleted: 1,
     })
 
     response.assertStatus(200)
     response.assertBodyContains({
-      title: 'Com ideia!',
+      title: 'Com ideias!',
       description: 'genial demais',
     })
   })
 
-  test('should test the destroy route', async ({ client }) => {
+  test('should test the destroy route', async ({ client }: { client: any }) => {
     const response = await client.delete('/toDos/3')
-
+    
     response.assertStatus(204)
   })
 })
