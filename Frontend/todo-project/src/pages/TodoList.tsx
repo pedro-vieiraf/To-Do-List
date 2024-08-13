703c052fa98480d3393ac76671648967f5da5727
Pedro Vieira <pedrovieirafdev@gmail.com>
2024-08-13 18:52:00 -0300

    configurações fullstack feitas

--- Frontend/todo-project/src/pages/TodoList.tsx
+++ Frontend/todo-project/src/pages/TodoList.tsx
@@ -6,19 +6,31 @@ function ToDoList() {
 
     useEffect(() => {
         async function getToDoList() {
-            const response = await fetch('http://backend:3333/toDos');
-            const data = await response.json();
-            setToDoList(data)
+            try {
+                console.log('Fetching data from http://backend:3333/toDos'); // Adicione este log
+                const response = await fetch('http://backend:3333/toDos');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                console.log('Data fetched:', data); // Adicione este log
+                setToDoList(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
         getToDoList();
     }, []);
 
     return (
-        <div>
-            {toDoList.map((toDo) => (
-                <div key={toDo}>{toDo}</div>
-            ))}
-        </div>
+        <>
+            <h1>Lista de Tarefas</h1>
+            <div>
+                {toDoList.map((toDo) => (
+                    <div key={toDo}>{toDo}</div>
+                ))}
+            </div>
+        </>
     )
 }
 
--- Frontend/todo-project/vite.config.ts
+++ Frontend/todo-project/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Adicione esta linha para escutar em todas as interfaces
+    port: 5173, // A porta na qual o Vite deve rodar
+  },
 })
